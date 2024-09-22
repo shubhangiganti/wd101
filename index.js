e3987adb7102087235e4d26267c15385e32871fe
shubhangiganti <gshubhangi2004@gmail.com>
2024-09-22 23:05:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,6 +70,11 @@ const saveForm = (event) => {
     return; // Stop form submission
   }*/
 
+  if (!form.checkValidity()) {
+    form.reportValidity(); // This will show the default browser error messages
+    return; // Stop the form submission
+  }
+
   if (!validate()) {
     alert("Your age must be between 18 and 55.");
     return;
